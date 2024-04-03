2c671a51983553f7e2d48c4cb3beb7cb8bee311e
ismailhelali <helaliismail@gmail.com>
2024-04-03 00:44:25 +0000

    podcast js

---
+++ podcast/mespodcast.js
@@ -0,0 +1,53 @@
+const podcasts = [
+    {
+        id: "vie",
+        link: "/podcast/img/lavie.png",
+        descriptionLink: "/podcast/blog.html" // Add description link
+    },
+    {
+        id: "vie",
+        link: "/podcast/img/lavie.png",
+        descriptionLink: "/podcast/blog.html" // Add description link
+    }
+];
+
+// Function to fetch description content asynchronously
+async function getDescriptionContent(descriptionLink) {
+    try {
+        const response = await fetch(descriptionLink);
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error(`Error fetching description: ${error.message}`);
+        return "Description not available";
+    }
+}
+
+// Fonction pour afficher les podcast
+async function afficherpodcasts() {
+    const container = document.getElementById('podcasts-containe');
+    for (const podcast of podcasts) {
+        const podcastElement = document.createElement('div');
+        mp3Element.classList.add('podcast');
+
+        // Fetch description content asynchronously
+        const descriptionContent = await getDescriptionContent(podcast.descriptionLink);
+
+        podcastElement.innerHTML = `
+        <div class="profile-container">
+        <div class="profile">
+            <img src="${podcast.link}" alt="Podcast Ismail helali titre La vie">
+        </div>
+        <a href="${descriptionLink.link}">
+        <div class="quote">La vie sous forme de Poesie - Ismail Helali</div></a>
+    </div>
+        `;
+        container.appendChild(podcastElement);
+    }
+}
+
+// Appel de la fonction pour afficher les services
+afficherpodcasts();
+
+
+
