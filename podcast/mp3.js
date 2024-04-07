6ae5c4e73d2038f7b1818bfbef26093c10a12740
ismailhelali <helaliismail@gmail.com>
2024-04-07 03:12:42 +0000

    mp3 css and js

--- podcast/mp3.js
+++ podcast/mp3.js
@@ -44,15 +44,28 @@ async function affichermp3s() {
                 const descriptionContent = await getDescriptionContent(mp3.descriptionLink);
                 mp3Element.innerHTML = `
 
-                <div class="service-card">
-                <audio src="${mp3.link}" controls></audio>
-                <div class="controls">
-                <div class="play-pause"></div>
+                <div class="audio-player">
+                <div id="play-btn"></div>
+                <div class="audio-wrapper" id="player-container" href="javascript:;">
+                  <audio id="player" ontimeupdate="initProgressBar()">
+                          <source src="${mp3.link}" type="audio/mp3">
+                        </audio>
                 </div>
-
+                <div class="player-controls scrubber">
+                  <p>${mp3.title} <small>by</small> ismail Helali</p>
+                  <span id="seekObjContainer">
+                          <progress id="seekObj" value="0" max="1"></progress>
+                        </span>
+                  <br>
+                  <small style="float: left; position: relative; left: 15px;" class="start-time"></small>
+                  <small style="float: right; position: relative; right: 20px;" class="end-time"></small>
+                
+                </div>
+                <div class="album-image" style="background-image: url('https://cdn.pixabay.com/audio/2024/01/15/16-58-27-753_200x200.jpg')"></div>
+                </div>
+                
                 <p>${descriptionContent}</p>
-              
-              
+                
                 `;
             })();
 
